4f0d467fde4b74de9c34ba4a2048b0db431ef6f3
joao costa <joaovaraujoc@gmail.com>
2022-05-12 14:45:29 -0300

    Pagination Layout

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,7 +1,7 @@
-function Listing(){
-    return(
-        <h1>Listing</h1>
-    );
+import Pagination from "components/Pagination";
+
+function Listing() {
+  return <Pagination />;
 }
 
 export default Listing;
